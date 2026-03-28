8fd6e5eb59a654514d9d3b8c727c660fb06a05c4
abraaorochapb <abraaorochapb@gmail.com>
2026-03-28 12:00:02 -0300

    Add calculations for total and relative frequencies of monster card types

--- central_tendency_measures/yugioh_ex.py
+++ central_tendency_measures/yugioh_ex.py
@@ -6,8 +6,22 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('https://raw.githubusercontent.com/fferegrino/yu-gi-oh/refs/heads/main/data/cards.csv')
 
 #%%
+# We use the unique() method to get the unique values in the 'type' column.
 df['type'].unique()
 
 #%%
+# We filter the DataFrame to include only rows where the 'type' column contains the word 'Monster'.
 df_monsters = df[df['type'].str.contains(r'\bMonster\b', na=False)]
 df_monsters.head()
+
+# %%
+# Here we use the count() method to get the total number of monster cards in the dataset.
+df_monsters['type'].count()
+# %%
+# Here we use the value_counts() method to get the count of each type of monster card in the dataset.
+df_monsters['type'].value_counts()
+
+# %%
+# Here we use the normalize parameter to get the relative frequencies of each type of monster card
+# then we multiply by 100 to convert it to percentages.
+df_monsters['type'].value_counts(normalize=True) * 100
