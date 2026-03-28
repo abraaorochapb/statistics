8df76f14d60833711f03982255c7a6917af1c918
abraaorochapb <abraaorochapb@gmail.com>
2026-03-28 11:44:02 -0300

    - filter and display unique card types and monster cards

--- central_tendency_measures/yugioh_ex.py
+++ central_tendency_measures/yugioh_ex.py
@@ -4,5 +4,10 @@ import matplotlib.pyplot as plt
 
 #%%
 df = pd.read_csv('https://raw.githubusercontent.com/fferegrino/yu-gi-oh/refs/heads/main/data/cards.csv')
-df.head()
 
+#%%
+df['type'].unique()
+
+#%%
+df_monsters = df[df['type'].str.contains(r'\bMonster\b', na=False)]
+df_monsters.head()
