9736e0b042936e8d34bca2573c92ed0b902f8656
abraaorochapb <abraaorochapb@gmail.com>
2026-03-28 11:28:36 -0300

    - Adds Requirements.txt - Adds first imports and defines the dataset we'll be using in the project

---
+++ central_tendency_measures/yugioh_ex.py
@@ -0,0 +1,8 @@
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#%%
+df = pd.read_csv('https://raw.githubusercontent.com/fferegrino/yu-gi-oh/refs/heads/main/data/cards.csv')
+df.head()
+
